25b5cade53f59da31cb3c40f2857ac835ab4e0b6
Nathan Terminal <Ufuomaodibo@hotmail.com>
2024-11-08 19:34:46 +0100

    loan

--- BankingApp/dto/loan.py
+++ BankingApp/dto/loan.py
@@ -5,9 +5,7 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 class LoanCreate(BaseModel):
-    user_id: int
+    user: int
     loan_amount: Decimal
     interest_rate: Decimal
     term_years: int
-    loan_balance: int
-    
--- BankingApp/routes/index.py
+++ BankingApp/routes/index.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from routes import user, deposit, transaction, withdraw,auth
+from routes import user, deposit, transaction, withdraw,auth,loan
 def register_routers(app: FastAPI):
     app.include_router(auth.router, prefix="/api")
     app.include_router(user.router, prefix="/api")
     app.include_router(deposit.router, prefix="/api")
     app.include_router(withdraw.router, prefix="/api")
     app.include_router(transaction.router, prefix="/api")
+    app.include_router(loan.router, prefix="/api")
+   
+   
+   
    
    
